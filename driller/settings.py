87f049a6c95902e2abfb2f38ee0dc866e4187840
itsafo <kolawoleafolabi13@gmail.com>
2020-09-08 02:52:21 +0100

    First commitment

--- driller/settings.py
+++ driller/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#q9j3y)12$(t#9nnaz(gj4!h=t+(!(pdc9vtz29n%j(hd(9bm)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'drilltech.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,9 +118,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+# This static root is defined to help heroku upload static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
+MEDIA_URL = '/images/'
 # Creating a location to store static files on any computer
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
