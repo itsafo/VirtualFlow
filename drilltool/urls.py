3edb7cdca4b43def2c9c619acf2de51a350da538
itsafo <kolawoleafolabi13@gmail.com>
2020-09-11 03:15:41 +0100

    Rheology Analytics

--- drilltool/urls.py
+++ drilltool/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 
 
 urlpatterns = [
-    path('', index, name='home')
+    path('', index, name='home'),
+    path('add', drill, name='add')
 ]
--- drilltool/views.py
+++ drilltool/views.py
@@ -9,3 +9,14 @@ def index(request):
     # One good old style of programming functions
     # Define your context variable, then render
     return render(request, "index.html", {})
+
+
+# +++++++++++++++ CALCULATION FUNCTIONS ++++++++++++++
+def drill(request):
+    # One good old style of programming functions
+    # Define your context variable, then render
+    if request.method == 'POST':
+	    val1 = int(request.POST['num1'])
+	    val2 = int(request.POST['num2'])
+	    res = val1 + val2
+    return render(request, "drill.html", {'result':res})
